aa28e61f8a15a9fb6169b86f2e623ec3f0876bbf
Mirjamkirschner <mirjam.kirschner@gmx.de>
2023-04-26 11:41:40 +0200

    json dokumente eingefügt

--- main.js
+++ main.js
@@ -32,3 +32,40 @@ L.marker([
 L.control.scale({
     imperial: false,
 }).addTo(map);
+
+//Vienna Sightseeing Haltestellen
+//asynchrone Funktion, damit schneller geladen werden kann
+async function showStops(url) {
+    let response = await fetch(url); //Anfrage, Antwort kommt zurück
+    let jsondata = await response.json (); //json Daten aus Response entnehmen 
+    L.geoJSON(jsondata).addTo(themaLayer.stops); //alle Busstopps anzeigen als Marker
+    //console.log(response);
+}
+showStops("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:TOURISTIKHTSVSLOGD&srsName=EPSG:4326&outputFormat=json"); //aufrufen der Funktion 
+
+async function showLines(url) {
+    let response = await fetch(url); 
+    let jsondata = await response.json ();
+    L.geoJSON(jsondata).addTo(themaLayer.lines); 
+    //console.log(response);
+}
+showLines("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:TOURISTIKLINIEVSLOGD&srsName=EPSG:4326&outputFormat=json");
+
+async function showSights(url) {
+    let response = await fetch(url); 
+    let jsondata = await response.json (); 
+    L.geoJSON(jsondata, {
+        onEachFeature: function(feature,layer){
+            let prop = feature.properties; //Variable damit kürzer; * steht als Platzhalter für Bildunterschrift, Link für Infos, nur 1 Tab für Links
+            layer.bindPopup(`
+            <img src = "${prop.THUMBNAIL}" alt="*" > 
+            <h4><a href="${prop.WEITERE_INF}" target="Wien">${prop.NAME}</a></h4>
+            <address>${prop.ADRESSE}</adress>
+            `);
+            console.log(prop.NAME);
+        }
+    }).addTo(themaLayer.sights); 
+    //console.log(response);
+}
+showSights("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:SEHENSWUERDIGOGD&srsName=EPSG:4326&outputFormat=json");
+
