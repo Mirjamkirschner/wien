ea45d4ef70832f83a58872c4185f63772afe899f
Mirjamkirschner <mirjam.kirschner@gmx.de>
2023-04-27 15:43:56 +0200

    Linien sichtbar machen und einzoomen

--- main.js
+++ main.js
@@ -10,14 +10,14 @@ let stephansdom = {
 // Karte initialisieren
 let map = L.map("map").setView([
     stephansdom.lat, stephansdom.lng
-], 12);
+], 15);
 
 //thematische Layer 
 let themaLayer = {
-    stops: L.featureGroup().addTo(map),
+    stops: L.featureGroup(),
     lines: L.featureGroup().addTo(map),
-    zones: L.featureGroup().addTo(map),
-    sights: L.featureGroup().addTo(map),
+    zones: L.featureGroup(),
+    sights: L.featureGroup(),
 }
 
 // Hintergrundlayer
